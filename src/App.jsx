24327cc39b424b4a2f61fe66ee4f91f81198b2f3
Pestañas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-01 09:27:49 -0600

    Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const Nombre = 'Alejandro Pestañas Vargas'
+  return (
+    <div class="position-absolute top-50 start-50 translate-middle">
+      <h3>Lista de Hobbies de {Nombre}</h3>
+      <ol>
+        <li>Dormir</li>
+        <li>Jugar</li>
+        <li>Comer</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
